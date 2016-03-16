03470cf435622309a27e8f9457735e83128503e9
Harendra Singh <harendra@shippable.com>
2016-03-16 16:12:15 +0530

    Initial commit
    Need to change to test cases

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+require("xunit-file");
+module.exports = function (grunt) {
+  grunt.initConfig({
+    express: {
+      test: {
+        options: {
+          script: "server.js"
+        }
+      }
+    },
+    simplemocha: {
+      options: {
+        globals: ["should"],
+        timeout: 3000,
+        ignoreLeaks: false,
+        ui: "bdd",
+        reporter: "xunit-file"
+      },
+      all: { src: ["./test.js"] }
+    }
+  });
+  grunt.loadNpmTasks("grunt-express-server");
+  grunt.loadNpmTasks("grunt-simple-mocha");
+  grunt.registerTask("default", ["express:test", "simplemocha:all"]);
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+Elastic BeanStalk Node.js Sample!
+=================
+
+A simple Node.js application with tests that will be deployed to [Amazon Elastic BeanStalk][1]
+
+Uses Grunt to run tests against an Express server, then generates reports with Xunit and Istanbul.
+
+This sample is built for Shippable, a docker based continuous integration and deployment platform.
+
+[1]: https://aws.amazon.com/elasticbeanstalk/
