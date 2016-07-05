da471b40af44c6a84c49ef75fe7f17bae24db24a
Tom Trahan <tom@shippable.com>
2016-07-05 15:56:06 -0700

    upgrade botocore version

--- prepare_build_folder.sh
+++ prepare_build_folder.sh
@@ -1,20 +1,11 @@
 #!/bin/bash -x
 
 mkdir -p build
-echo $?
 cp -r public build/
-echo $?
 cp -r tests build/
-echo $?
 cp .gitignore build/
-echo $?
-cp .nightwatch.json build/
-echo $?
+cp nightwatch.json build/
 cp package.json build/
-echo $?
 cp README.md build/
-echo $?
 cp server.js build/
-echo $?
 cp test.js build/
-echo $?
